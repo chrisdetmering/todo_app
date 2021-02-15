80bbe638c27b7f2f2762628cbf7738193ca152e5
BryanGK <bryangordonkrauss@gmail.com>
2021-02-15 07:52:39 -0800

    The toDo is mostly working. Items not clearing off localStorage correctly. Still need to add strikethough on complete.

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ const done = document.querySelectorAll('.pending');
 submitBtn.addEventListener('click', () => {
     let listItem = toDo.value;
     localStorage.setItem(`toDo${localStorage.length + 1}`, listItem);
-    
 })
 
-    for (let i = 1; i <= localStorage.length; i++) {
-        const newListItem = document.createElement('li');
-        const doneButton = addButtonDone(i);
-        const clearButton = addButtonClear(i);
-        newListItem.append(localStorage.getItem(`toDo${i}`));
-        newListItem.append(doneButton);
-        newListItem.append(clearButton);
-        toDoList.append(newListItem);
-        console.log(localStorage.getItem(`toDo${i}`));
-    }
+for (let i = 1; i <= localStorage.length; i++) {
+    const newListItem = document.createElement('li');
+    const doneButton = addButtonDone(i);
+    const clearButton = addButtonClear(i);
+    newListItem.append(localStorage.getItem(`toDo${i}`));
+    newListItem.append(doneButton);
+    newListItem.append(clearButton);
+    newListItem.classList.add('list-item')
+    toDoList.append(newListItem);
+    console.log(localStorage.getItem(`toDo${i}`));
+}
 
 function addButtonDone(i) {
-    const doneButton = document.createElement('button');
-    doneButton.innerHTML = "Done";
+    const doneButton = document.createElement('input');
+    doneButton.setAttribute('type', 'checkbox');
     doneButton.classList.add('pending');
     return doneButton;
 }
@@ -31,17 +31,31 @@ function addButtonDone(i) {
 function addButtonClear(i) {
     const clearButton = document.createElement('button');
     clearButton.innerHTML = "Clear";
-    clearButton.classList.add('clear', `toDo${i}`);
+    clearButton.classList.add('clear');
+    clearButton.setAttribute('id', `${i}`);
+    clearButton.addEventListener('click', clearList)
     return clearButton;
 }
 
 const clear = document.querySelectorAll('.clear');
 
-clear.forEach(function(elem) {
-    elem.addEventListener('click', () => {
-            
-        }
-    );
-});
+function clearList(index) {
+    localStorage.removeItem(localStorage.key(index));
+    this.closest('.list-item').remove();
+    console.log('clear');
+}
+
+// function removeLocalStorage(item) {
+//     let i = localStorage.length;
+//     while (i-- > 0) {
+//         let key = localStorage.key(i);
+//         if (localStorage.getItem(key) === item) {
+//             localStorage.removeItem(key);
+//         }
+//     }
+// }
+
+
+
 
 
